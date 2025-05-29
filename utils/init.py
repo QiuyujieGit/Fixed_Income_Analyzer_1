bd84de67c61836040a9c7d68f817e3a4a0dfb493
QiuyujieGit <53214468+QiuyujieGit@users.noreply.github.com>
2025-05-29 15:42:31 +0800

    Update init.py

--- utils/init.py
+++ utils/init.py
@@ -1,6 +1,10 @@
 """工具模块"""
 from .file_handler import FileHandler
 from .logger import setup_logger
-from .data_processor import DataProcessor
 
-__all__ = ['FileHandler', 'setup_logger', 'DataProcessor']
+# 如果data_processor.py存在，添加：
+try:
+    from .data_processor import DataProcessor
+    __all__ = ['FileHandler', 'setup_logger', 'DataProcessor']
+except ImportError:
+    __all__ = ['FileHandler', 'setup_logger']
